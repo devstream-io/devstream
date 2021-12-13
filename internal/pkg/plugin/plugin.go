6f3fd84722df0862e3b3d350eb85cc755161c875
Tiexin Guo <guotiexin@gmail.com>
2021-12-13 17:39:34 +0800

    refactor: changes according to the refactor of plugin manager

--- internal/pkg/plugin/plugin.go
+++ internal/pkg/plugin/plugin.go
@@ -6,7 +6,6 @@ import (
 	"plugin"
 
 	"github.com/merico-dev/stream/internal/pkg/config"
-	"github.com/merico-dev/stream/internal/pkg/download"
 )
 
 // DevStreamPlugin is a struct, on which install/reinstall/uninstall interfaces are defined.
@@ -14,6 +13,24 @@ type DevStreamPlugin interface {
 	Install(*map[string]interface{})
 	Reinstall(*map[string]interface{})
 	Uninstall(*map[string]interface{})
+}
+
+// Install loads the plugin and calls the Install method of that plugin.
+func Install(tool *config.Tool) {
+	p := loadPlugin(tool)
+	p.Install(&tool.Options)
+}
+
+// Reinstall loads the plugin and calls the Reinstall method of that plugin.
+func Reinstall(tool *config.Tool) {
+	p := loadPlugin(tool)
+	p.Reinstall(&tool.Options)
+}
+
+// Uninstall loads the plugin and calls the Uninstall method of that plugin.
+func Uninstall(tool *config.Tool) {
+	p := loadPlugin(tool)
+	p.Uninstall(&tool.Options)
 }
 
 func loadPlugin(tool *config.Tool) DevStreamPlugin {
@@ -38,43 +55,4 @@ func loadPlugin(tool *config.Tool) DevStreamPlugin {
 	}
 
 	return devStreamPlugin
-}
-
-// Install loads the plugin and calls the Install method of that plugin.
-func Install(tool *config.Tool) {
-
-	path := fmt.Sprintf("plugins/%s_%s.so", tool.Name, tool.Version)
-	appname := fmt.Sprintf("%s_%s.so", tool.Name, tool.Version)
-	if !FileExist(path) {
-		loader := download.NewDownloadClient()
-		loader.AssetName = appname
-		loader.Version = tool.Version
-		loader.Filepath = path
-		err := loader.GetAssetswithretry()
-		if err != nil {
-			fmt.Println(err)
-			os.Exit(1)
-		}
-
-	}
-
-	p := loadPlugin(tool)
-	p.Install(&tool.Options)
-}
-
-// Reinstall loads the plugin and calls the Reinstall method of that plugin.
-func Reinstall(tool *config.Tool) {
-	p := loadPlugin(tool)
-	p.Reinstall(&tool.Options)
-}
-
-// Uninstall loads the plugin and calls the Uninstall method of that plugin.
-func Uninstall(tool *config.Tool) {
-	p := loadPlugin(tool)
-	p.Uninstall(&tool.Options)
-}
-
-func FileExist(path string) bool {
-	_, err := os.Lstat(path)
-	return !os.IsNotExist(err)
 }
