e220fc13505da8971f7ac9e9d086b1294839beee
Daniel Hu <tao.hu@merico.dev>
2022-01-17 11:12:03 +0800

    Feat: Deleting the dead ut code for githubactions
    Signed-off-by: Daniel Hu <tao.hu@merico.dev>

--- internal/pkg/plugin/githubactions/githubactions_test.go
+++ internal/pkg/plugin/githubactions/githubactions_test.go
@@ -1,43 +1,9 @@
 package githubactions
 
-//Notice: run local only
+import (
+	. "github.com/onsi/ginkgo/v2"
+)
 
-//import (
-//	. "github.com/onsi/ginkgo/v2"
-//	. "github.com/onsi/gomega"
-//	"gopkg.in/yaml.v3"
-//)
-//
-//var _ = Describe("Githubactions", func() {
-//	var optionsStr = `owner: daniel-hutao
-//repo: animal
-//language:
-//  name: go
-//  version: "1.17"
-//branch: master
-//jobs:
-//  build:
-//    enable: True
-//    command: "go build ./..."
-//  test:
-//    enable: True
-//    command: "go test ./..."
-//    coverage:
-//      enable: False
-//      profile: "-race -coverprofile=coverage.out -covermode=atomic"
-//`
-//	var options *map[string]interface{}
-//
-//	BeforeEach(func() {
-//		  err := yaml.Unmarshal([]byte(optionsStr), &options)
-//		  Expect(err).To(Equal(nil))
-//	})
-//
-//	Context("for IsHealthy()", func() {
-//		It("should return (true, nil)", func() {
-//			healthy, err := IsHealthy(options)
-//			Expect(err).To(Equal(nil))
-//			Expect(healthy).To(Equal(true))
-//		})
-//	})
-//})
+var _ = Describe("Githubactions", func() {
+	// TODO(daniel-hutao): implement the unit tests
+})
