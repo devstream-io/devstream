68c53dee8e2286fd148e03fe812b1641392d8155
lfbdev <fangbao.li@merico.dev>
2022-01-20 00:16:52 +0800

    logging improvement

--- internal/pkg/util/log/log.go
+++ internal/pkg/util/log/log.go
@@ -15,30 +15,37 @@ const (
 	FATAL   = "[FATAL] "
 )
 
+// Success log success with color and symbol, for a success operation
 func Success(format string, args ...interface{}) {
 	cligger.Success(color.Success.Render(SUCCESS)+"%s", fmt.Sprint(args...))
 }
 
+// Info log info with color and symbol, for a notice
 func Info(format string, args ...interface{}) {
 	cligger.Info(color.Blue.Render(INFO)+"%s", fmt.Sprint(args...))
 }
 
+// Warn log warn with color and symbol, for a warning event
 func Warn(format string, args ...interface{}) {
 	cligger.Warning(color.Warn.Render(WARN)+"%s", fmt.Sprint(args...))
 }
 
+// Errorf log error with color,symbol and format for a warning event
 func Errorf(format string, args ...interface{}) {
 	cligger.Error(color.Error.Render(ERROR)+format, args...)
 }
 
+// Error log error with color adn symbol for a warning event
 func Error(args ...interface{}) {
 	cligger.Error(color.Error.Render(ERROR)+"%s", fmt.Sprint(args...))
 }
 
+// Fatalf log fatal with color,symbol and format for a fatal event
 func Fatalf(format string, args ...interface{}) {
 	cligger.Fatal(color.BgRed.Render(FATAL)+format, args...)
 }
 
+// Fatal log fatal with color and symbol for a fatal event
 func Fatal(args ...interface{}) {
 	cligger.Fatal(color.BgRed.Render(FATAL)+"%s", fmt.Sprint(args...))
 }
