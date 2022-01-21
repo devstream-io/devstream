6f9343596d7547391f6ea9112750d9cb83d5e007
Daniel Hu <tao.hu@merico.dev>
2022-01-21 14:02:58 +0800

    Feat: Adding util/github & util/downloader (#149)
    Signed-off-by: Daniel Hu <tao.hu@merico.dev>

---
+++ internal/pkg/util/downloader/downloader_test.go
@@ -0,0 +1,27 @@
+package downloader_test
+
+import (
+	"os"
+
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+
+	"github.com/merico-dev/stream/internal/pkg/util/downloader"
+)
+
+var _ = Describe("Downloader", func() {
+	Context("Downloader test", func() {
+		var url = "https://github.com/merico-dev/stream/releases/download/v0.0.1/argocdapp_0.0.1.so"
+		var targetDir = "tmp"
+		It("Should get the file", func() {
+			size, err := downloader.Download(url, targetDir)
+			Expect(err).NotTo(HaveOccurred())
+			Expect(size).NotTo(Equal(int64(0)))
+		})
+
+		AfterEach(func() {
+			err := os.RemoveAll(targetDir)
+			Expect(err).NotTo(HaveOccurred())
+		})
+	})
+})
---
+++ internal/pkg/util/github/github_suite_test.go
@@ -0,0 +1,13 @@
+package github_test
+
+import (
+	"testing"
+
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+)
+
+func TestPlanmanager(t *testing.T) {
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "GitHub Suite")
+}
