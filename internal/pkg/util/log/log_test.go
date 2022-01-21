124a444e4b9b0487160f73fcb21e0c9ed76ccc86
lfbdev <fangbao.li@merico.dev>
2022-01-21 11:50:51 +0800

    using logrus framework

--- internal/pkg/util/log/log_test.go
+++ internal/pkg/util/log/log_test.go
@@ -8,14 +8,11 @@ func TestLog(t *testing.T) {
 	Debug("nice to meet you ", "log")
 	Info("nice to meet you ", "log")
 	Warn("nice to meet you ", "log")
-	Error("nice to meet you ", "log")
 	Success("nice to meet you ", "log")
 
 	Debugf("nice to meet you %s", "log")
 	Infof("nice to meet you %s", "log")
 	Warnf("nice to meet you %s", "log")
-	Errorf("nice to meet you %s", "log")
 	Successf("nice to meet you %s", "log")
-	Fatalf("nice to meet you %s", "log")
 
 }
