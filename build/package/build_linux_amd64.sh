9ff0dff63475f27cf78bca6b9efbde2957d266a5
lfbdev <fangbao.li@merico.dev>
2022-01-24 11:44:23 +0800

    update

--- build/package/build_linux_amd64.sh
+++ build/package/build_linux_amd64.sh
@@ -4,7 +4,7 @@ set -e
 
 export GOOS=linux
 export GOARCH=amd64
-export VERSION=0.0.1
+VERSION
 go build -trimpath -gcflags="all=-N -l" -o output/dtm-${GOOS}-${GOARCH} ./cmd/devstream/
 go build -buildmode=plugin -trimpath -gcflags="all=-N -l" -o output/githubactions-${GOOS}-${GOARCH}_${VERSION}.so ./cmd/githubactions/
 go build -buildmode=plugin -trimpath -gcflags="all=-N -l" -o output/trello-github-integ-${GOOS}-${GOARCH}_${VERSION}.so ./cmd/trellogithub/
