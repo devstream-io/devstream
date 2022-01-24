52ce396547afc6924ec401a6214115cf04548483
lfbdev <fangbao.li@merico.dev>
2022-01-24 10:41:08 +0800

    dockerfile update and comment

--- internal/pkg/version/version.go
+++ internal/pkg/version/version.go
@@ -1,6 +1,6 @@
 package version
 
-// VERSION is the version of DevStream.
+// VERSION is the version of DevStream (from -ldflags of Makefile, if miss, 0.0.1 is default version).
 var VERSION string
 
 // REVISION is the revision of DevStream.
