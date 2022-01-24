72e57c6acf777021f9fb09b9630bf05d03ced576
Fangbao Li <94163234+lfbdev@users.noreply.github.com>
2022-01-24 13:57:37 +0800

    Makefile version improve (#155)
    * add version var for build
    
    * Update version.go
    
    * Update build_linux_amd64.sh
    
    * dockerfile update and comment
    
    * update
    
    * update
    
    * update
    
    * uddate dockerfile
    
    * update
    
    * add a blank

--- internal/pkg/version/version.go
+++ internal/pkg/version/version.go
@@ -1,7 +1,7 @@
 package version
 
-// VERSION is the version of DevStream.
-var VERSION = "0.0.1"
+// VERSION is the version of DevStream (from -ldflags of Makefile, if miss, 0.0.1 is default version).
+var VERSION string
 
 // REVISION is the revision of DevStream.
 var REVISION = "unknown"
