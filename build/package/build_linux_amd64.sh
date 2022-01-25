33caae9d8808d1d450776a8fea29addd3532f438
Daniel Hu <tao.hu@merico.dev>
2022-01-25 15:49:13 +0800

    Fix: kube-prometheus pkg name error (#162)
    Signed-off-by: Daniel Hu <tao.hu@merico.dev>

--- build/package/build_linux_amd64.sh
+++ build/package/build_linux_amd64.sh
@@ -10,5 +10,5 @@ go build -buildmode=plugin -trimpath -gcflags="all=-N -l" -o output/githubaction
 go build -buildmode=plugin -trimpath -gcflags="all=-N -l" -o output/trello-github-integ-${GOOS}-${GOARCH}_${VERSION}.so ./cmd/trellogithub/
 go build -buildmode=plugin -trimpath -gcflags="all=-N -l" -o output/argocd-${GOOS}-${GOARCH}_${VERSION}.so ./cmd/argocd/
 go build -buildmode=plugin -trimpath -gcflags="all=-N -l" -o output/argocdapp-${GOOS}-${GOARCH}_${VERSION}.so ./cmd/argocdapp/
-go build -buildmode=plugin -trimpath -gcflags="all=-N -l" -o output/kube-prometheus-${GOOS}-${GOARCH}_${VERSION}.so ./cmd/kube-prometheus/
+go build -buildmode=plugin -trimpath -gcflags="all=-N -l" -o output/kube-prometheus-${GOOS}-${GOARCH}_${VERSION}.so ./cmd/kubeprometheus/
 go build -buildmode=plugin -trimpath -gcflags="all=-N -l" -o output/github-repo-scaffolding-golang-${GOOS}-${GOARCH}_${VERSION}.so ./cmd/reposcaffolding/github/golang/
