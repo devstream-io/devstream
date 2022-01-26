3499ac2337510846eb733661067cd3d3586afeed
Daniel Hu <tao.hu@merico.dev>
2022-01-26 15:12:55 +0800

    Feat: Replace viper.GetString() with os.Getenv() in util/trello
    Signed-off-by: Daniel Hu <tao.hu@merico.dev>

--- pkg/util/trello/trello.go
+++ pkg/util/trello/trello.go
@@ -2,9 +2,9 @@ package trello
 
 import (
 	"fmt"
+	"os"
 
 	"github.com/adlio/trello"
-	"github.com/spf13/viper"
 )
 
 type Client struct {
@@ -13,8 +13,8 @@ type Client struct {
 
 func NewClient() (*Client, error) {
 	helpUrl := "https://docs.servicenow.com/bundle/quebec-it-asset-management/page/product/software-asset-management2/task/generate-trello-apikey-token.html"
-	apiKey := viper.GetString("TRELLO_API_KEY")
-	token := viper.GetString("TRELLO_TOKEN")
+	apiKey := os.Getenv("TRELLO_API_KEY")
+	token := os.Getenv("TRELLO_TOKEN")
 	if apiKey == "" || token == "" {
 		return nil, fmt.Errorf("TRELLO_API_KEY and/or TRELLO_TOKEN are/is empty. see %s for more info", helpUrl)
 	}
