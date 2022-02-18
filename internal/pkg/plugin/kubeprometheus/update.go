fa9a11890e1da0d930339e535f44270688fd48a8
Daniel Hu <tao.hu@merico.dev>
2022-02-18 13:39:19 +0800

    Feat: simplify the Update() with helm-style plugins. (#205)
    Signed-off-by: Daniel Hu <tao.hu@merico.dev>

--- internal/pkg/plugin/kubeprometheus/update.go
+++ internal/pkg/plugin/kubeprometheus/update.go
@@ -1,55 +1,15 @@
 package kubeprometheus
 
 import (
-	"fmt"
-
-	"github.com/mitchellh/mapstructure"
-
 	"github.com/merico-dev/stream/internal/pkg/log"
-	"github.com/merico-dev/stream/pkg/util/helm"
 )
 
 // Update updates kube-prometheus with provided options.
 func Update(options *map[string]interface{}) (map[string]interface{}, error) {
-	var param Param
-	if err := mapstructure.Decode(*options, &param); err != nil {
-		return nil, err
-	}
-
-	if errs := validate(&param); len(errs) != 0 {
-		for _, e := range errs {
-			log.Errorf("Param error: %s", e)
-		}
-		return nil, fmt.Errorf("params are illegal")
-	}
-
-	h, err := helm.NewHelm(param.GetHelmParam())
+	_, err := Delete(options)
 	if err != nil {
+		log.Errorf("Failed to delete the kube-prometheus: %s", err)
 		return nil, err
 	}
-
-	log.Info("Uninstalling kube-prometheus-stack helm chart ...")
-	if err = h.UninstallHelmChartRelease(); err != nil {
-		return nil, err
-	}
-
-	if err := dealWithNsWhenDelete(&param); err != nil {
-		return nil, err
-	}
-
-	// install
-	if err := dealWithNsWhenInstall(&param); err != nil {
-		return nil, err
-	}
-
-	log.Info("Installing or updating kube-prometheus-stack helm chart ...")
-	if err = h.InstallOrUpgradeChart(); err != nil {
-		return nil, err
-	}
-
-	releaseName := param.Chart.ReleaseName
-	retMap := GetStaticState(releaseName).ToStringInterfaceMap()
-	log.Debugf("Return map: %v", retMap)
-
-	return retMap, nil
+	return Create(options)
 }
