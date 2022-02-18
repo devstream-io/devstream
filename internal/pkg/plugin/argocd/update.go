c3600fc6e5cda5dc8e5b613feea5e67d43135146
Daniel Hu <tao.hu@merico.dev>
2022-02-18 11:08:35 +0800

    Feat: simplify the Update() with helm-style plugins.
    Signed-off-by: Daniel Hu <tao.hu@merico.dev>

--- internal/pkg/plugin/argocd/update.go
+++ internal/pkg/plugin/argocd/update.go
@@ -1,53 +1,14 @@
 package argocd
 
 import (
-	"fmt"
-
-	"github.com/mitchellh/mapstructure"
-
 	"github.com/merico-dev/stream/internal/pkg/log"
-	"github.com/merico-dev/stream/pkg/util/helm"
 )
 
 func Update(options *map[string]interface{}) (map[string]interface{}, error) {
-	var param Param
-	if err := mapstructure.Decode(*options, &param); err != nil {
-		return nil, err
-	}
-
-	if errs := validate(&param); len(errs) != 0 {
-		for _, e := range errs {
-			log.Errorf("Param error: %s", e)
-		}
-		return nil, fmt.Errorf("params are illegal")
-	}
-
-	h, err := helm.NewHelm(param.GetHelmParam())
+	_, err := Delete(options)
 	if err != nil {
+		log.Errorf("Failed to delete the ArgoCD: %s", err)
 		return nil, err
 	}
-
-	log.Info("Uninstalling argocd helm chart ...")
-	if err = h.UninstallHelmChartRelease(); err != nil {
-		return nil, err
-	}
-
-	if err := dealWithNsWhenUninstall(&param); err != nil {
-		return nil, err
-	}
-
-	// install
-	if err := dealWithNsWhenInstall(&param); err != nil {
-		return nil, err
-	}
-
-	log.Info("Installing or updating argocd helm chart ...")
-	if err = h.InstallOrUpgradeChart(); err != nil {
-		return nil, err
-	}
-
-	retMap := GetStaticState().ToStringInterfaceMap()
-	log.Debugf("Return map: %v", retMap)
-
-	return retMap, nil
+	return Create(options)
 }
