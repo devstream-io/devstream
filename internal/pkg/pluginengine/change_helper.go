6a46cb30bda102cc3e34d152c6d5d6baca5847da
Tiexin Guo <guotiexin@gmail.com>
2022-02-22 16:39:35 +0800

    Feat apply plan refactor (#211)
    * refactor: apply/delete refactor according to state/return result change
    
    * bug: fix ci-lint issues
    
    * feat: store tool options into state
    
    * chore: merge ssot branch

--- internal/pkg/pluginengine/change_helper.go
+++ internal/pkg/pluginengine/change_helper.go
@@ -27,8 +27,8 @@ func generateAction(tool *configloader.Tool, action statemanager.ComponentAction
 	}
 }
 
-func drifted(r map[string]interface{}, s *statemanager.State) bool {
-	return !cmp.Equal(r, s.Resource)
+func drifted(a, b map[string]interface{}) bool {
+	return !cmp.Equal(a, b)
 }
 
 // changesForApply is to filter all the Tools in cfg that need some actions
@@ -37,31 +37,38 @@ func changesForApply(smgr statemanager.Manager, statesMap statemanager.StatesMap
 	for _, tool := range cfg.Tools {
 		state := smgr.GetState(getStateKeyFromTool(&tool))
 
-		// config not found in state
 		if state == nil {
-			// no need to Read resource before Create
+			// tool not in state, create, no need to Read resource before Create
 			changes = append(changes, generateCreateAction(&tool))
 			log.Infof("Change added: %s -> %s", tool.Name, statemanager.ActionCreate)
-			continue
-		}
-
-		// config found in state / state isn't empty; trying to read resource status
-		resource, err := Read(&tool)
-		if err != nil {
-			return changes, err
-		}
-
-		if resource == nil {
-			// state has it but resource doesn't exist; need to create
-			changes = append(changes, generateCreateAction(&tool))
-			log.Infof("Change added: %s -> %s", tool.Name, statemanager.ActionCreate)
-		} else if drifted(resource, state) {
-			// resource drifted from state, need to update
-			changes = append(changes, generateUpdateAction(&tool))
-			log.Infof("Change added: %s -> %s", tool.Name, statemanager.ActionUpdate)
 		} else {
-			// resource is the same as the state, do nothing
-			log.Debugf("Tool < %s > -> state == resource, not drifted, do nothing.", tool.Name)
+			// tool found in state
+			if drifted(tool.Options, state.Options) {
+				// tool's config differs from State's, Update
+				changes = append(changes, generateUpdateAction(&tool))
+				log.Infof("Change added: %s -> %s", tool.Name, statemanager.ActionUpdate)
+			} else {
+				// tool's config is the same as State's
+
+				// read resource status
+				resource, err := Read(&tool)
+				if err != nil {
+					return changes, err
+				}
+
+				if resource == nil {
+					// tool exists in state, but resource doesn't exist, Create
+					changes = append(changes, generateCreateAction(&tool))
+					log.Infof("Change added: %s -> %s", tool.Name, statemanager.ActionCreate)
+				} else if drifted(resource, state.Resource) {
+					// resource drifted from state, Update
+					changes = append(changes, generateUpdateAction(&tool))
+					log.Infof("Change added: %s -> %s", tool.Name, statemanager.ActionUpdate)
+				} else {
+					// resource is the same as the state, do nothing
+					log.Debugf("Tool %s state and resource are the same, not drifted, do nothing.", tool.Name)
+				}
+			}
 		}
 
 		statesMap.Delete(getStateKeyFromTool(&tool))
