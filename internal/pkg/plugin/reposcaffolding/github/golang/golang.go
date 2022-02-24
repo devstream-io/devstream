4dcdd4f8f7afcab1a09a7a325bf81c7944c80e9a
Tiexin Guo <guotiexin@gmail.com>
2022-02-24 21:56:24 +0800

    Bug fix for v0.2.0 (#229)
    * bug: fix repo owner lower/upper case issue for repo scaffolding for github golang
    
    * chore: add more debug log

--- internal/pkg/plugin/reposcaffolding/github/golang/golang.go
+++ internal/pkg/plugin/reposcaffolding/github/golang/golang.go
@@ -182,7 +182,7 @@ func replaceAppNameInPathStr(filePath, appName string) (string, error) {
 
 func buildState(param *Param) map[string]interface{} {
 	res := make(map[string]interface{})
-	res["owner"] = param.Owner
+	res["owner"] = strings.ToLower(param.Owner)
 	res["repoName"] = param.Repo
 	return res
 }
--- internal/pkg/plugin/reposcaffolding/github/golang/read.go
+++ internal/pkg/plugin/reposcaffolding/github/golang/read.go
@@ -2,6 +2,7 @@ package golang
 
 import (
 	"fmt"
+	"strings"
 
 	"github.com/mitchellh/mapstructure"
 
@@ -44,7 +45,7 @@ func buildReadState(param *Param) (map[string]interface{}, error) {
 	}
 
 	res := make(map[string]interface{})
-	res["owner"] = *repo.Owner.Login
+	res["owner"] = strings.ToLower(*repo.Owner.Login)
 	res["repoName"] = *repo.Name
 
 	return res, nil
