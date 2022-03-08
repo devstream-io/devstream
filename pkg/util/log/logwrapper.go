b6e7e1bc22a647dad6acb61af0c4347b7b641c4d
lfbdev <fangbao.li@merico.dev>
2022-03-08 10:16:01 +0800

    fix: log stack print for error

--- pkg/util/log/logwrapper.go
+++ pkg/util/log/logwrapper.go
@@ -3,6 +3,7 @@ package log
 import (
 	"bytes"
 	"fmt"
+	"runtime"
 
 	"github.com/sirupsen/logrus"
 	"gopkg.in/gookit/color.v1"
@@ -16,6 +17,12 @@ const (
 	ERROR   = "[ERROR] "
 	FATAL   = "[FATAL] "
 )
+
+// ROOTCALLER should be main proc
+const ROOTCALLER = "main.main"
+
+// REMOVELEVEL logrus stack level is 9, should be removed from stack trace
+const REMOVELEVEL = 10
 
 type CliLoggerFormatter struct {
 	level           logrus.Level
@@ -32,9 +39,15 @@ func (m *CliLoggerFormatter) Format(entry *logrus.Entry) ([]byte, error) {
 	} else {
 		b = &bytes.Buffer{}
 	}
+
 	m.levelPrintRender()
 
 	timestamp := entry.Time.Format("2006-01-02 15:04:05")
+
+	if m.level == logrus.ErrorLevel {
+		entry.Message = addCallStackIgnoreLogrus(entry.Message)
+	}
+
 	newLog := fmt.Sprintf("%s %s %s %s\n", timestamp, m.prefix, m.formatLevelName, entry.Message)
 
 	b.WriteString(newLog)
@@ -81,12 +94,28 @@ func (s *SeparatorFormatter) Format(entry *logrus.Entry) ([]byte, error) {
 	} else {
 		b = &bytes.Buffer{}
 	}
+
 	timestamp := entry.Time.Format("2006-01-02 15:04:05")
 	newLog := fmt.Sprintf("%s %s %s %s\n",
 		timestamp,
 		color.Blue.Render(normal.Info),
 		color.Blue.Render(INFO),
 		color.Blue.Render(fmt.Sprintf("%s %s %s", "-------------------- [ ", entry.Message, " ] --------------------")))
+
 	b.WriteString(newLog)
 	return b.Bytes(), nil
+}
+
+// addCallStackIgnoreLogrus add call stack to log message without logrus stack
+func addCallStackIgnoreLogrus(rawMessage string) string {
+	stackMessage := rawMessage
+	for i := REMOVELEVEL; ; i++ {
+		pc, file, line, _ := runtime.Caller(i)
+		stackMessage = stackMessage + "\n  -- " + file + fmt.Sprintf(" %d", line)
+		entrance := runtime.FuncForPC(pc).Name()
+		if entrance == ROOTCALLER {
+			break
+		}
+	}
+	return stackMessage
 }
