e851a9f2e4edf744602353f2f978d74937710406
Daniel Hu <tao.hu@merico.dev>
2022-03-19 13:58:01 +0800

    feat(helm-style plugins): update without delete
    Signed-off-by: Daniel Hu <tao.hu@merico.dev>

--- internal/pkg/plugin/openldap/update.go
+++ internal/pkg/plugin/openldap/update.go
@@ -1,18 +1,36 @@
 package openldap
 
 import (
-	"time"
+	"fmt"
 
+	"github.com/mitchellh/mapstructure"
+
+	. "github.com/merico-dev/stream/internal/pkg/plugin/common/helm"
 	"github.com/merico-dev/stream/pkg/util/log"
 )
 
 func Update(options map[string]interface{}) (map[string]interface{}, error) {
-	_, err := Delete(options)
-	if err != nil {
-		log.Errorf("Failed to delete the OpenLDAP: %s.", err)
+	// 1. decode options
+	var param Param
+	if err := mapstructure.Decode(options, &param); err != nil {
 		return nil, err
 	}
 
-	<-time.NewTicker(3 * time.Second).C
-	return Create(options)
+	if errs := validate(&param); len(errs) != 0 {
+		for _, e := range errs {
+			log.Errorf("Param error: %s.", e)
+		}
+		return nil, fmt.Errorf("params are illegal")
+	}
+
+	// 2. install or upgrade
+	if err := InstallOrUpgradeChart(&param); err != nil {
+		return nil, err
+	}
+
+	// 3. fill the return map
+	retMap := GetStaticState().ToStringInterfaceMap()
+	log.Debugf("Return map: %v", retMap)
+
+	return retMap, nil
 }
