1dd6ee00c9c46e9c8bfbf738a2a1013b86140546
Tiexin Guo <tiexin.guo@merico.dev>
2022-04-28 15:18:51 +0800

    feat: gitlab commits/projects related functions

--- pkg/util/gitlab/commits.go
+++ pkg/util/gitlab/commits.go
@@ -2,6 +2,8 @@ package gitlab
 
 import (
 	"github.com/xanzy/go-gitlab"
+
+	"github.com/devstream-io/devstream/pkg/util/log"
 )
 
 // https://docs.gitlab.com/ee/api/commits.html
@@ -62,6 +64,32 @@ func (c *Client) UpdateSingleFile(project, branch, commitMessage, filename, cont
 	}
 
 	_, _, err := c.Commits.CreateCommit(project, &createCommitoptions)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (c *Client) CommitMultipleFiles(project, branch, commitMessage string, files map[string][]byte) error {
+	var commitActionsOptions = make([]*gitlab.CommitActionOptions, 0)
+
+	for fileName, content := range files {
+		commitActionsOptions = append(commitActionsOptions, &gitlab.CommitActionOptions{
+			Action:   gitlab.FileAction(gitlab.FileCreate),
+			FilePath: gitlab.String(fileName),
+			Content:  gitlab.String(string(content)),
+		})
+	}
+
+	createCommitOptions := gitlab.CreateCommitOptions{
+		Branch:        gitlab.String(branch),
+		CommitMessage: gitlab.String(commitMessage),
+		Actions:       commitActionsOptions,
+	}
+
+	_, response, err := c.Commits.CreateCommit(project, &createCommitOptions)
+	log.Debug(response.Body)
 	if err != nil {
 		return err
 	}
