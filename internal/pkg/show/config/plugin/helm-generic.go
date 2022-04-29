1ae088977853b76623cbeb1b0e6eeb6872b7bb5c
lfbdev <fangbao.li@merico.dev>
2022-04-29 15:56:36 +0800

    fix: helm generic show content

--- internal/pkg/show/config/plugin/helm-generic.go
+++ internal/pkg/show/config/plugin/helm-generic.go
@@ -12,25 +12,25 @@ var HelmGenericDefaultConfig = `tools:
     # need to create the namespace or not, default: false
     create_namespace: true
     repo:
-      # name of the Helm repo
-      name: argo
-      # url of the Helm repo
-      url: https://argoproj.github.io/argo-helm
+      # name of the Helm repo, e.g. argo
+      name: HELM_REPO_NAME
+      # url of the Helm repo, e.g. https://argoproj.github.io/argo-helm
+      url: YOUR_CHART_REPO
     # Helm chart information
     chart:
-      # name of the chart
-      chart_name: argo/argo-cd
-      # release name of the chart
-      release_name: argocd
-      # k8s namespace
-      namespace: argocd
+      # name of the chart, e.g. argo/argo-cd
+      chart_name: CHART_NAME
+      # release name of the chart, e.g. argocd
+      release_name: RELEASE_NAME
+      # k8s namespace, e.g. argocd
+      namespace: YOUR_CHART_NAMESPACE
       # whether to wait for the release to be deployed or not
       wait: true
       # the time to wait for any individual Kubernetes operation (like Jobs for hooks). This defaults to 5m0s
       timeout: 5m
       # whether to perform a CRD upgrade during installation
       upgradeCRDs: true
-      # custom configuration (Optional). You can refer to [ArgoCD values.yaml](https://github.com/argoproj/argo-helm/blob/master/charts/argo-cd/values.yaml)
+      # custom configuration (Optional). e.g. You can refer to [ArgoCD values.yaml](https://github.com/argoproj/argo-helm/blob/master/charts/argo-cd/values.yaml)
       values_yaml: |
         controller:
           service: 
