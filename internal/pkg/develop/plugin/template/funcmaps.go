4f8d97859a65fe335b15cca0a35cc209bbaf1bb6
summingyu <summingbest@gmail.com>
2022-05-19 20:56:29 +0800

    refactor(develop): add special_plugins.go

--- internal/pkg/develop/plugin/template/funcmaps.go
+++ internal/pkg/develop/plugin/template/funcmaps.go
@@ -2,28 +2,6 @@ package template
 
 import "strings"
 
-type SpecialPlugin struct {
-	DirName     string
-	PackageName string
-}
-
-func NewSpecialPlugin(dirName string, packageName string) *SpecialPlugin {
-	return &SpecialPlugin{
-		DirName:     dirName,
-		PackageName: packageName,
-	}
-}
-
-var SpecialPluginNameMap = map[string]*SpecialPlugin{
-	"gitlabci-golang":                NewSpecialPlugin("gitlabci/golang", "gitlabci"),
-	"gitlabci-generic":               NewSpecialPlugin("gitlabci/generic", "generic"),
-	"gitlab-repo-scaffolding-golang": NewSpecialPlugin("reposcaffolding/github/golang", "golang"),
-	"github-repo-scaffolding-golang": NewSpecialPlugin("reposcaffolding/gitlab/golang", "golang"),
-	"githubactions-golang":           NewSpecialPlugin("githubactions/golang", "golang"),
-	"githubactions-nodejs":           NewSpecialPlugin("githubactions/nodejs", "nodejs"),
-	"githubactions-python":           NewSpecialPlugin("githubactions/python", "python"),
-}
-
 func FormatPackageName(name string) string {
 	if specialPluginName, isOk := SpecialPluginNameMap[name]; isOk {
 		return specialPluginName.PackageName
