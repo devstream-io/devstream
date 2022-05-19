ff6aa827f0cb9cae9e2d71bf47303266192e5996
yclchuxue <75575291+yclchuxue@users.noreply.github.com>
2022-05-19 22:06:07 +0800

    feat: auto-release-darwin-arm64.sh improvement
    * feat: auto-release-darwin-arm64.sh improvement
    
    * chore: some minor improvement with auto-release-darwin-arm64.sh
    
    Signed-off-by: Daniel Hu <tao.hu@merico.dev>
    
    Co-authored-by: yclchuxue <yclchuxue@gmail.com>
    Co-authored-by: Daniel Hu <tao.hu@merico.dev>

--- hack/release/auto-release-darwin-arm64.sh
+++ hack/release/auto-release-darwin-arm64.sh
@@ -1,7 +1,27 @@
 #! /bin/bash -e
+# usage: `sh auto-release-darwin-arm64.sh -t v0.6.0`
 set -o nounset
 
-tag=v0.5.0
+tag="invalid"
+
+while getopts "t:" opt; do
+  case $opt in
+  t)
+    tag=$OPTARG
+    ;;
+
+  ?)
+    echo "Options not used"
+    exit 1
+    ;;
+  esac
+done
+
+if [ "${tag}" == "invalid" ]; then
+  echo "Maybe you forgot to use -t flag. E.g. sh auto-release-darwin-arm64.sh -t v0.6.0"
+  exit 1
+fi
+echo "tag: ${tag}"
 
 user=devstream-io
 repo=devstream
@@ -32,8 +52,6 @@ make build -j8
 # install github-release for uploading
 go install github.com/github-release/github-release@latest
 
-
-
 # upload dtm
 echo 'Uploading 'dtm-${GOOS}-${GOARCH}' ...'
 github-release upload --security-token $github_token --user $user --repo $repo --tag $tag --file dtm --name dtm-${GOOS}-${GOARCH}
@@ -42,4 +60,4 @@ echo dtm-${GOOS}-${GOARCH}' uploaded.'
 # upload plugins and .md5 files
 # In order to upload plug-ins to s3, you need to download awscli. After downloading awscli, you need to configure aws credentials.
 pip3 install awscli
-aws s3 cp $plugin_dir  s3://download.devstream.io/${tag} --recursive --acl public-read
+aws s3 cp $plugin_dir s3://download.devstream.io/${tag} --recursive --acl public-read
