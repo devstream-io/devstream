7de11dad33f3c39ed636fe30adf5b357013cf58a
iyear <ljyngup@gmail.com>
2022-05-19 12:28:23 +0800

    fix: jiragithub options required `JiraBaseUrl` `JiraUserEmail` `JiraProjectKey`

--- internal/pkg/plugin/jiragithub/options.go
+++ internal/pkg/plugin/jiragithub/options.go
@@ -5,8 +5,8 @@ type Options struct {
 	Owner          string `validate:"required_without=Org"`
 	Org            string `validate:"required_without=Owner"`
 	Repo           string `validate:"required"`
-	JiraBaseUrl    string
-	JiraUserEmail  string
-	JiraProjectKey string
+	JiraBaseUrl    string `validate:"required"`
+	JiraUserEmail  string `validate:"required"`
+	JiraProjectKey string `validate:"required"`
 	Branch         string `validate:"required"`
 }
