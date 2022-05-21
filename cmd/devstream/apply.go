dcfa67acebc7118508d68099f7e7754605051d7e
Shubham Gupta <69793468+shubham-cmyk@users.noreply.github.com>
2022-05-21 19:55:38 +0530

    Update mine

--- cmd/devstream/apply.go
+++ cmd/devstream/apply.go
@@ -9,6 +9,9 @@ import (
 	"github.com/devstream-io/devstream/pkg/util/log"
 )
 
+var configFile string 
+
+
 var applyCMD = &cobra.Command{
 	Use:   "apply",
 	Short: "Create or update DevOps tools according to DevStream configuration file",
@@ -16,6 +19,17 @@ var applyCMD = &cobra.Command{
 DevStream will generate and execute a new plan based on the config file and the state file by default.`,
 	Run: applyCMDFunc,
 }
+
+var applyConfigCMD = &cobra.Command{
+	Use:   "config",
+	Short: "Show configuration information",
+	Long: `Show config is used for showing plugins' template configuration information.
+Examples:
+  dtm show config --plugin=A-PLUGIN-NAME`,
+	Run: applyConfigCMDFunc,
+}
+
+
 
 func applyCMDFunc(cmd *cobra.Command, args []string) {
 	log.Info("Apply started.")
@@ -25,3 +39,30 @@ func applyCMDFunc(cmd *cobra.Command, args []string) {
 	}
 	log.Success("Apply finished.")
 }
+
+
+func applyconfigCMDfunc(cmd *cobra.Command, args []string){
+	
+//	log.Debug("Show configuration information.")
+	if err := config.Apply(); err != nil {
+		log.Fatal(err)
+	}
+}
+
+
+func init() {
+	
+	applyCMD.AddCommand(applyCMD)
+	applyCMD.PersistentFlags().StringVarP(&configFile, "config-file", "f", "config.yaml", "config file")
+	viper.BindPFlag("confgFile", applyCmd.PersistentFlags().Lookup("configFile"))
+
+	
+}
+
+func initconfig(){
+	viper.AutomaticEnv()
+	if err := viper.BindPFlags( applyCMD.Flags()); err != nil {
+		log.Fatal(err)
+	}
+}
+
