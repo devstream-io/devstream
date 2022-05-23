fb9e8b04114babbef0ce01e0133de85db896820f
esonhugh <esonhughoutside@gmail.com>
2022-05-23 12:19:46 +0800

    refactor(command test): add comments and rename

--- test/evil_test.go
+++ test/evil_test.go
@@ -9,21 +9,30 @@ import (
 
 func TestExecInSystem(t *testing.T) {
 	log := bytes.NewBuffer([]byte{})
+	// normal inject
 	paramsYouThink := "echo This Normal"
 	paramsYouThink = paramsYouThink + ";cat /etc/passwd;echo This is accident."
-	os.ExecInSystem(".", paramsYouThink, log, true) // Unsafe allow injections but allow more.
+	os.ExecInSystem(".", paramsYouThink, log, true) // Unsafe allow injections but allow complex commands when hardcode.
+
+	// inject to params
 	paramsYouThink2 := []string{
 		"-al",
 		".",
 		";cat /etc/passwd;echo This is accident.",
 	}
 	os.SafeExecInSystem(".", "ls", paramsYouThink2, log, true)                           // Blocked
+
+	// inject to cmdName
 	os.SafeExecInSystem(".", "echo bad Commands;cat /etc/passwd", []string{}, log, true) // Blocked
+
+	// Try to introduce a shell command
 	os.SafeExecInSystem(".", "sh", []string{
 		"-c",
 		";cat /etc/passwd;echo This is accident.",
 	}, log, true) // Blocked
 	params := "--help"
+
+	// Try to test normal func.
 	os.SafeExecInSystem(".", "kubectl", []string{
 		params,
 	}, log, true) // Success.
