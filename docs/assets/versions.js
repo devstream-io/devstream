d1ced0529d0c64187c99a69238f588bb8a2c1756
Tiexin Guo <guotiexin@gmail.com>
2022-05-25 11:24:10 +0800

    chore: reformat

--- docs/assets/versions.js
+++ docs/assets/versions.js
@@ -32,28 +32,27 @@ setTimeout(function() {
     document.getElementsByTagName('head')[0].appendChild(script);
   }, 0);
   
-  // VERSION WARNINGS
-  window.addEventListener("DOMContentLoaded", function() {
-    var rtdData = window['READTHEDOCS_DATA'] || { version: 'latest' };
-    var margin = 30;
-    var headerHeight = document.getElementsByClassName("md-header")[0].offsetHeight; 
-    if (rtdData.version === "latest") {
-      document.querySelector("div[data-md-component=announce]").innerHTML = "<div id='announce-msg'>You are viewing the latest of DevStream docs. Maybe you want to choose a specific version.</a></div>"
-      var bannerHeight = document.getElementById('announce-msg').offsetHeight + margin
-      document.querySelector("header.md-header").style.top = bannerHeight +"px";   
-      document.querySelector('style').textContent +=
-      "@media screen and (min-width: 76.25em){ .md-sidebar { height: 0;  top:"+ (bannerHeight+headerHeight)+"px !important; }}"
-      document.querySelector('style').textContent +=
-      "@media screen and (min-width: 60em){ .md-sidebar--secondary { height: 0;  top:"+ (bannerHeight+headerHeight)+"px !important; }}"
-    }
-    // else if (rtdData.version !== "stable") {
-    //   document.querySelector("div[data-md-component=announce]").innerHTML = "<div id='announce-msg'>You are viewing the docs for a previous version of DevStream, <a href='https://devstream.readthedocs.io/en/stable/'>click here to go to the latest stable version.</a></div>"
-    //   var bannerHeight = document.getElementById('announce-msg').offsetHeight + margin
-    //   document.querySelector("header.md-header").style.top = bannerHeight +"px";   
-    //   document.querySelector('style').textContent +=
-    //   "@media screen and (min-width: 76.25em){ .md-sidebar { height: 0;  top:"+ (bannerHeight+headerHeight)+"px !important; }}"
-    //   document.querySelector('style').textContent +=
-    //   "@media screen and (min-width: 60em){ .md-sidebar--secondary { height: 0;  top:"+ (bannerHeight+headerHeight)+"px !important; }}"
-    // }
-  });
-  
+// VERSION WARNINGS
+window.addEventListener("DOMContentLoaded", function() {
+var rtdData = window['READTHEDOCS_DATA'] || { version: 'latest' };
+var margin = 30;
+var headerHeight = document.getElementsByClassName("md-header")[0].offsetHeight; 
+if (rtdData.version === "latest") {
+    document.querySelector("div[data-md-component=announce]").innerHTML = "<div id='announce-msg'>You are viewing the latest of DevStream docs. Maybe you want to choose a specific version.</a></div>"
+    var bannerHeight = document.getElementById('announce-msg').offsetHeight + margin
+    document.querySelector("header.md-header").style.top = bannerHeight +"px";   
+    document.querySelector('style').textContent +=
+    "@media screen and (min-width: 76.25em){ .md-sidebar { height: 0;  top:"+ (bannerHeight+headerHeight)+"px !important; }}"
+    document.querySelector('style').textContent +=
+    "@media screen and (min-width: 60em){ .md-sidebar--secondary { height: 0;  top:"+ (bannerHeight+headerHeight)+"px !important; }}"
+}
+// else if (rtdData.version !== "stable") {
+//   document.querySelector("div[data-md-component=announce]").innerHTML = "<div id='announce-msg'>You are viewing the docs for a previous version of DevStream, <a href='https://devstream.readthedocs.io/en/stable/'>click here to go to the latest stable version.</a></div>"
+//   var bannerHeight = document.getElementById('announce-msg').offsetHeight + margin
+//   document.querySelector("header.md-header").style.top = bannerHeight +"px";   
+//   document.querySelector('style').textContent +=
+//   "@media screen and (min-width: 76.25em){ .md-sidebar { height: 0;  top:"+ (bannerHeight+headerHeight)+"px !important; }}"
+//   document.querySelector('style').textContent +=
+//   "@media screen and (min-width: 60em){ .md-sidebar--secondary { height: 0;  top:"+ (bannerHeight+headerHeight)+"px !important; }}"
+// }
+});
