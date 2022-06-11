9aeaac6f089cdbb2b90c6d817df356f20793b578
summingyu <summingbest@gmail.com>
2022-06-11 08:37:40 +0800

    test(downloader): add downloader test case (#666)
    * test(downloader): add downloader test case
    
    1. improve coverage
    2. optimize time to error
    3. update test cases description
    
    Co-authored-by: Tiexin Guo <tiexin.guo@merico.dev>

--- pkg/util/downloader/downloader.go
+++ pkg/util/downloader/downloader.go
@@ -14,26 +14,25 @@ import (
 func Download(url, filename, targetDir string) (int64, error) {
 	log.Debugf("Target dir: %s.", targetDir)
 	log.Debugf("URL: %s.", url)
-	resp, err := http.Get(url)
-	if err != nil {
-		return 0, err
+	if filename == "" {
+		// when url is empty filepath.Base(url) will return "."
+		filename = filepath.Base(url)
 	}
-
-	var fileName = filename
-	if fileName == "" {
-		fileName = filepath.Base(url)
-	}
-	log.Debugf("Filename: %s.", fileName)
-	if fileName == "." {
+	log.Debugf("Filename: %s.", filename)
+	if filename == "." {
 		return 0, fmt.Errorf("failed to get the filename from url: %s", url)
 	}
 
-	err = os.MkdirAll(targetDir, 0755)
+	if err := os.MkdirAll(targetDir, 0755); err != nil {
+		return 0, err
+	}
+
+	f, err := os.Create(filepath.Join(targetDir, filename))
 	if err != nil {
 		return 0, err
 	}
 
-	f, err := os.Create(filepath.Join(targetDir, fileName))
+	resp, err := http.Get(url)
 	if err != nil {
 		return 0, err
 	}
--- pkg/util/downloader/downloader_test.go
+++ pkg/util/downloader/downloader_test.go
@@ -13,7 +13,20 @@ var _ = Describe("Downloader", func() {
 	Context("Downloader test", func() {
 		var url = "https://github.com/devstream-io/devstream/releases/download/v0.0.1/argocdapp_0.0.1.so"
 		var targetDir = "tmp"
-		It("Should get the file", func() {
+
+		It("returns error if filename is empty and can't be parsed from the url", func() {
+			size, err := downloader.Download(url, ".", targetDir)
+			Expect(err).To(HaveOccurred())
+			Expect(size).To(Equal(int64(0)))
+		})
+
+		It("returns an error when url and filename are empty", func() {
+			size, err := downloader.Download("", "", targetDir)
+			Expect(err).To(HaveOccurred())
+			Expect(size).To(Equal(int64(0)))
+		})
+
+		It("should download the file properly", func() {
 			size, err := downloader.Download(url, "", targetDir)
 			Expect(err).NotTo(HaveOccurred())
 			Expect(size).NotTo(Equal(int64(0)))
