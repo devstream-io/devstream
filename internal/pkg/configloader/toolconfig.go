cb5a09729dbdac3800329b619f48c89a0411fac2
sjie <baemawu@gmail.com>
2022-06-23 17:03:29 +0800

    commit configLoad ut examples

--- internal/pkg/configloader/toolconfig.go
+++ internal/pkg/configloader/toolconfig.go
@@ -2,11 +2,13 @@ package configloader
 
 import (
 	"fmt"
-	"github.com/devstream-io/devstream/pkg/util/validator"
 	"io/ioutil"
 
-	"github.com/devstream-io/devstream/pkg/util/log"
+	"github.com/devstream-io/devstream/pkg/util/validator"
+
 	"gopkg.in/yaml.v3"
+
+	"github.com/devstream-io/devstream/pkg/util/log"
 )
 
 // Tool is the struct for one section of the DevStream tool file (part of the config.)
