7a73fc3a08cdd66535bfa4bb1922e6a6be061e49
xian-jie.shen <327411586@qq.com>
2022-07-15 12:01:51 +0800

    perf: improve the FillMapWithStrAndError performance and split too long test case
    Signed-off-by: xian-jie.shen <327411586@qq.com>

--- pkg/util/mapz/map.go
+++ pkg/util/mapz/map.go
@@ -1,7 +1,7 @@
 package mapz
 
 func FillMapWithStrAndError(keys []string, value error) map[string]error {
-	retMap := make(map[string]error)
+	retMap := make(map[string]error, len(keys))
 	if len(keys) == 0 {
 		return retMap
 	}
