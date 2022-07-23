fd70d83017cfd8758450d4b83e4f24d12262242d
Bird <aflybird0@gmail.com>
2022-07-23 17:21:12 +0800

    feat: use `gojenkins` and complete `read`, `update`, `delete` interface
    Signed-off-by: Bird <aflybird0@gmail.com>

--- internal/pkg/plugin/jenkinspipelinekubernetes/jenkinspipelinekubernetes.go
+++ internal/pkg/plugin/jenkinspipelinekubernetes/jenkinspipelinekubernetes.go
@@ -1,9 +1,30 @@
 package jenkinspipelinekubernetes
 
-// TODO(aFlyBird0): specify the resource fields here.
+import (
+	_ "embed"
+
+	"github.com/devstream-io/devstream/pkg/util/jenkins"
+)
+
+//go:embed job-template.xml
+var jobTemplate string
+
+// NewJenkinsFromOptions creates a Jenkins client from the given options and test the connection.
+func NewJenkinsFromOptions(opts *Options) (*jenkins.Jenkins, error) {
+	return jenkins.NewJenkins(opts.J.URL, opts.J.User, opts.J.Password)
+}
+
+// TODO(aFlyBird0): enhance the resource fields here to be read and the way to read it, such as:
+// plugins install info(GitHub Pull Request Builder Plugin and OWASP Markup Formatter must be installed)
+// should we keep an eye on job configuration && status changes? maybe not.
 type resource struct {
+	CredentialsCreated bool
+	JobCreated         bool
 }
 
 func (res *resource) toMap() map[string]interface{} {
-	return map[string]interface{}{}
+	return map[string]interface{}{
+		"credentialsCreated": res.CredentialsCreated,
+		"jobCreated":         res.JobCreated,
+	}
 }
---
+++ pkg/util/jenkins/client.go
@@ -0,0 +1,106 @@
+package jenkins
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/bndr/gojenkins"
+)
+
+const (
+	domain          = "_"
+	credentialScope = "GLOBAL"
+)
+
+type (
+	// Jenkins is a Jenkins client,
+	// it is a wrapper around gojenkins.Jenkins and implements additional methods
+	Jenkins struct {
+		*gojenkins.Jenkins
+
+		*BasicAuth
+		JenkinsURL string
+	}
+
+	BasicAuth struct {
+		Username string
+		Password string
+	}
+)
+
+// NewJenkins creates a new Jenkins client and validates the connection
+func NewJenkins(jenkinsURL, username, password string) (*Jenkins, error) {
+	// validate
+	if jenkinsURL == "" {
+		return nil, fmt.Errorf("jenkinsURL is required")
+	}
+	if username == "" || password == "" {
+		return nil, fmt.Errorf("username and password are required")
+	}
+
+	// create gojenkins client
+	gojenkinsClient := gojenkins.CreateJenkins(nil, jenkinsURL, username, password)
+	ctx := context.Background()
+	// init and validate client
+	if _, err := gojenkinsClient.Init(ctx); err != nil {
+		return nil, err
+	}
+
+	return &Jenkins{
+		Jenkins: gojenkinsClient,
+		BasicAuth: &BasicAuth{
+			Username: username,
+			Password: password,
+		},
+		JenkinsURL: jenkinsURL,
+	}, nil
+}
+
+func (j *Jenkins) GetCredentialManager() *gojenkins.CredentialsManager {
+	return &gojenkins.CredentialsManager{
+		J: j.Jenkins,
+	}
+}
+
+func (j *Jenkins) CreateCredentialsUsername(id, description string) error {
+	cred := gojenkins.UsernameCredentials{
+		ID:          id,
+		Scope:       credentialScope,
+		Username:    j.Username,
+		Password:    j.Password,
+		Description: description,
+	}
+
+	// create credential
+	ctx := context.Background()
+	cm := j.GetCredentialManager()
+	err := cm.Add(ctx, domain, cred)
+	if err != nil {
+		return fmt.Errorf("could not create credential: %v", err)
+	}
+
+	// get credential to validate creation
+	getCred := gojenkins.UsernameCredentials{}
+	if err = cm.GetSingle(ctx, domain, cred.ID, &getCred); err != nil {
+		return fmt.Errorf("could not get credential: %v", err)
+	}
+
+	return nil
+}
+
+// GetCredentialsUsername returns the credentials of type username-password with the given id,
+// it returns an error if the credential does not exist
+func (j *Jenkins) GetCredentialsUsername(id string) (*gojenkins.UsernameCredentials, error) {
+	getCred := gojenkins.UsernameCredentials{}
+	ctx := context.Background()
+	cm := j.GetCredentialManager()
+	err := cm.GetSingle(ctx, domain, id, &getCred)
+	if err != nil {
+		return nil, fmt.Errorf("could not get credential: %v", err)
+	}
+	return &getCred, nil
+}
+
+func (j *Jenkins) DeleteCredentialsUsername(id string) error {
+	return j.GetCredentialManager().Delete(context.Background(), domain, id)
+}
