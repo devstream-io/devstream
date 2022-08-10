118e72cd4100d314fca23f08ad5ce109f1b9bbe9
Bird <aflybird0@gmail.com>
2022-08-10 10:29:04 +0800

    refactor: use go embed to generate templates
    Signed-off-by: Bird <aflybird0@gmail.com>

--- internal/pkg/develop/plugin/template/NAME_plugin.md.go
+++ internal/pkg/develop/plugin/template/NAME_plugin.md.go
@@ -1,26 +1,18 @@
 package template
 
-import "fmt"
+import _ "embed"
 
 var NAME_plugin_md_nameTpl = "{{ .Name }}.md"
 var NAME_plugin_md_dirTpl = "docs/plugins/"
 
-var NAME_plugin_md_contentTpl = `# {{ .Name }} plugin
-
-TODO(dtm): Add your document here.
-
-## Usage
-
-%s yaml
---8<-- "{{ .Name }}.yaml"
-%s
-`
+//go:embed NAME_plugin.md.tpl
+var NAME_plugin_md_contentTpl string
 
 func init() {
 
 	TplFiles = append(TplFiles, TplFile{
 		NameTpl:    NAME_plugin_md_nameTpl,
 		DirTpl:     NAME_plugin_md_dirTpl,
-		ContentTpl: fmt.Sprintf(NAME_plugin_md_contentTpl, "```", "```"),
+		ContentTpl: NAME_plugin_md_contentTpl,
 	})
 }
--- internal/pkg/develop/plugin/template/NAME_plugin.zh.md.go
+++ internal/pkg/develop/plugin/template/NAME_plugin.zh.md.go
@@ -1,26 +1,18 @@
 package template
 
-import "fmt"
+import _ "embed"
 
 var NAME_plugin_zh_md_nameTpl = "{{ .Name }}.zh.md"
 var NAME_plugin_zh_md_dirTpl = "docs/plugins/"
 
-var NAME_plugin_zh_md_contentTpl = `# {{ .Name }} 插件
-
-TODO(dtm): 在这里添加文档.
-
-## 用例
-
-%s yaml
---8<-- "{{ .Name }}.yaml"
-%s
-`
+//go:embed NAME_plugin.zh.md.tpl
+var NAME_plugin_zh_md_contentTpl string
 
 func init() {
 
 	TplFiles = append(TplFiles, TplFile{
 		NameTpl:    NAME_plugin_zh_md_nameTpl,
 		DirTpl:     NAME_plugin_zh_md_dirTpl,
-		ContentTpl: fmt.Sprintf(NAME_plugin_zh_md_contentTpl, "```", "```"),
+		ContentTpl: NAME_plugin_zh_md_contentTpl,
 	})
 }
