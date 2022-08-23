759662012815c5286ece313202ce07ee47889af7
Daniel Hu <tao.hu@merico.dev>
2022-08-23 20:39:58 +0800

    feat: jenkins plugin jenkins_url improvement
    Signed-off-by: Daniel Hu <tao.hu@merico.dev>

--- internal/pkg/plugin/jenkins/jenkins.go
+++ internal/pkg/plugin/jenkins/jenkins.go
@@ -1,6 +1,9 @@
 package jenkins
 
 import (
+	"fmt"
+	"strings"
+
 	"github.com/devstream-io/devstream/internal/pkg/plugininstaller"
 	"github.com/devstream-io/devstream/internal/pkg/plugininstaller/helm"
 	"github.com/devstream-io/devstream/internal/pkg/statemanager"
@@ -29,20 +32,46 @@ func genJenkinsState(options plugininstaller.RawOptions) (statemanager.ResourceS
 		return nil, err
 	}
 
-	// svc_name.svc_ns:svc_port
-	outputs := map[string]interface{}{
-		"jenkins_url": "http://jenkins.jenkins:8080",
-	}
-	resState.SetOutputs(outputs)
-
 	// values.yaml
 	opt, err := helm.NewOptions(options)
 	if err != nil {
 		return nil, err
 	}
-
 	valuesYaml := opt.GetHelmParam().Chart.ValuesYaml
 	resState["values_yaml"] = valuesYaml
 
+	svcName, err := genJenkinsSvcName(options)
+	if err != nil {
+		return nil, err
+	}
+
+	// svc_name.svc_ns:svc_port
+	url := fmt.Sprintf("http://%s.%s:8080", svcName, opt.Chart.Namespace)
+	outputs := map[string]interface{}{
+		"jenkins_url": url,
+	}
+
+	resState.SetOutputs(outputs)
+
 	return resState, nil
+}
+
+// see https://github.com/devstream-io/devstream/pull/1025#discussion_r952277174 for more info.
+func genJenkinsSvcName(options plugininstaller.RawOptions) (string, error) {
+	opts, err := helm.NewOptions(options)
+	if err != nil {
+		return "", err
+	}
+
+	var retStr string
+	if strings.Contains(opts.Chart.ChartName, opts.Chart.ReleaseName) {
+		retStr = strings.TrimSuffix(opts.Chart.ReleaseName, "-")
+		if len(retStr) > 63 {
+			retStr = retStr[:64]
+		}
+		return retStr, nil
+	}
+	retStr = fmt.Sprintf("%s-%s", opts.Chart.ReleaseName, opts.Chart.ChartName)
+
+	return retStr, nil
 }
