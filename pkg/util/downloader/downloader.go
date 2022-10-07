0928d70113bac1fe437dc07c3238ab1c40751504
Zheyu-Tian <Isupremei@outlook.com>
2022-10-07 19:45:58 +0800

    feat: add process bar during `dtm upgrade`
    Signed-off-by: 0zyt <zyt0@duck.com>

--- pkg/util/downloader/downloader.go
+++ pkg/util/downloader/downloader.go
@@ -1,12 +1,15 @@
 package downloader
 
 import (
-	"bytes"
 	"fmt"
 	"io"
 	"net/http"
 	"os"
 	"path/filepath"
+	"strconv"
+	"time"
+
+	"github.com/cheggaaa/pb"
 
 	"github.com/devstream-io/devstream/pkg/util/log"
 )
@@ -39,11 +42,7 @@ func Download(url, filename, targetDir string) (int64, error) {
 			log.Debugf("download create file failed: %s", err)
 		}
 	}()
-	content, err := FetchContentFromURL(url)
-	if err != nil {
-		return 0, err
-	}
-	return io.Copy(f, bytes.NewBuffer(content))
+	return FetchContentFromURLAndSetUpProgressBar(url, f)
 }
 
 func FetchContentFromURL(url string) ([]byte, error) {
@@ -66,4 +65,44 @@ func FetchContentFromURL(url string) ([]byte, error) {
 	}
 
 	return io.ReadAll(resp.Body)
+}
+
+// setUpProgressBar create bar and setup
+func SetUpProgressBar(resp *http.Response, downFile io.Writer) (int64, error) {
+	//get size
+	i, _ := strconv.Atoi(resp.Header.Get("Content-Length"))
+	sourceSiz := int64(i)
+	source := resp.Body
+
+	//create a bar and set param
+	bar := pb.New(int(sourceSiz)).SetRefreshRate(time.Millisecond * 10).SetUnits(pb.U_BYTES).SetWidth(100)
+	bar.ShowSpeed = true
+	bar.ShowTimeLeft = true
+	bar.ShowFinalTime = true
+	bar.SetWidth(80)
+	bar.Start()
+	defer bar.Finish()
+	writer := io.MultiWriter(downFile, bar)
+	return io.Copy(writer, source)
+}
+
+func FetchContentFromURLAndSetUpProgressBar(url string, dst io.Writer) (int64, error) {
+	resp, err := http.Get(url)
+
+	// check response error
+	if err != nil {
+		log.Debugf("Download from url failed: %s", err)
+		return 0, err
+	}
+	defer func(body io.ReadCloser) {
+		err := body.Close()
+		if err != nil {
+			log.Errorf("Close response body failed: %s", err)
+		}
+	}(resp.Body)
+
+	if resp.StatusCode != http.StatusOK {
+		return 0, fmt.Errorf("Download file from url failed: %+v", resp)
+	}
+	return SetUpProgressBar(resp, dst)
 }
