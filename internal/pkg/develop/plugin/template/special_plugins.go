0cb55ef0662b72a876b4aaeff734208fe7a2c64a
Daniel Hu <tao.hu@merico.dev>
2022-10-27 15:35:59 +0800

    fix: gitlabci-xxx validation failed
    Signed-off-by: Daniel Hu <tao.hu@merico.dev>

--- internal/pkg/develop/plugin/template/special_plugins.go
+++ internal/pkg/develop/plugin/template/special_plugins.go
@@ -13,7 +13,8 @@ func NewSpecialPlugin(dirName string, packageName string) *SpecialPlugin {
 }
 
 var SpecialPluginNameMap = map[string]*SpecialPlugin{
-	"gitlabci-golang":      NewSpecialPlugin("gitlabci/golang", "gitlabci"),
+	"gitlabci-golang":      NewSpecialPlugin("gitlabci/golang", "golang"),
+	"gitlabci-java":        NewSpecialPlugin("gitlabci/java", "java"),
 	"gitlabci-generic":     NewSpecialPlugin("gitlabci/generic", "generic"),
 	"githubactions-golang": NewSpecialPlugin("githubactions/golang", "golang"),
 	"githubactions-nodejs": NewSpecialPlugin("githubactions/nodejs", "nodejs"),
