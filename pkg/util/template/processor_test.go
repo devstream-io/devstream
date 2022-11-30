b551edc6d2f9b872cd662c779221b729600c5985
Bird <aflybird0@gmail.com>
2022-11-30 16:29:22 +0800

    feat: support env vars
    Signed-off-by: Bird <aflybird0@gmail.com>

--- pkg/util/template/processor_test.go
+++ pkg/util/template/processor_test.go
@@ -1,27 +1,149 @@
 package template
 
 import (
-	"testing"
-
-	"github.com/stretchr/testify/assert"
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
 )
 
-func TestAddDotForVariablesInConfigNormal(t *testing.T) {
-	res := addDotForVariablesInConfig("[[varNameA]]")
-	assert.Equal(t, "[[ .varNameA]]", res, "Adding dot for variable names passed.")
-}
+var _ = Describe("addDotForVariablesInConfig", func() {
+	var (
+		origin, gotten, expected string
+	)
 
-func TestAddDotForVariablesInConfigWithSpaces(t *testing.T) {
-	res := addDotForVariablesInConfig("[[  varNameA]]")
-	assert.Equal(t, "[[ .varNameA]]", res, "Adding dot for variable names passed.")
-}
+	JustBeforeEach(func() {
+		gotten = addDotForVariablesInConfig(origin)
+	})
 
-func TestAddDotForVariablesInConfigWithTrailingSpaces(t *testing.T) {
-	res := addDotForVariablesInConfig("[[ varNameA  ]]")
-	assert.Equal(t, "[[ .varNameA  ]]", res, "Adding dot for variable names passed.")
-}
+	When("config is normal", func() {
+		BeforeEach(func() {
+			origin = "[[varNameA]]"
+			expected = "[[ .varNameA ]]"
+		})
+		It("should succeed", func() {
+			Expect(gotten).To(Equal(expected))
+		})
+	})
 
-func TestAddDotForVariablesInConfigMultipleVars(t *testing.T) {
-	res := addDotForVariablesInConfig("[[ varNameA ]]/[[ varNameB ]]/[[ varNameC ]]")
-	assert.Equal(t, "[[ .varNameA ]]/[[ .varNameB ]]/[[ .varNameC ]]", res, "Adding dot for variable names passed.")
-}
+	When("config has spaces", func() {
+		BeforeEach(func() {
+			origin = "[[ varNameA ]]"
+			expected = "[[ .varNameA ]]"
+		})
+
+		It("should succeed", func() {
+			Expect(gotten).To(Equal(expected))
+		})
+	})
+
+	When("config has trailing spaces", func() {
+		BeforeEach(func() {
+			origin = "[[ varNameA  ]]"
+			expected = "[[ .varNameA ]]"
+		})
+
+		It("should succeed", func() {
+			Expect(gotten).To(Equal(expected))
+		})
+	})
+
+	When("config has multiple variables", func() {
+		BeforeEach(func() {
+			origin = "[[ varNameA ]]/[[ varNameB ]]/[[ varNameC ]]"
+			expected = "[[ .varNameA ]]/[[ .varNameB ]]/[[ .varNameC ]]"
+		})
+
+		It("should succeed", func() {
+			Expect(gotten).To(Equal(expected))
+		})
+	})
+
+	When("there are more than one words", func() {
+		BeforeEach(func() {
+			origin = "[[ func varNameA ]]"
+			expected = origin
+		})
+
+		It("should do nothing", func() {
+			Expect(gotten).To(Equal(expected))
+		})
+	})
+})
+
+var _ = Describe("addQuoteForVariablesInConfig", func() {
+	var (
+		origin, gotten, expected string
+	)
+
+	JustBeforeEach(func() {
+		gotten = addQuoteForVariablesInConfig(origin)
+	})
+	AfterEach(func() {
+		Expect(gotten).To(Equal(expected))
+	})
+
+	When("config is normal", func() {
+		BeforeEach(func() {
+			origin = `[[env GITHUB_TOKEN]]`
+			expected = `[[ env "GITHUB_TOKEN" ]]`
+		})
+
+		It("should succeed", func() {
+			Expect(gotten).To(Equal(expected))
+		})
+	})
+
+	When("config has single quote", func() {
+		BeforeEach(func() {
+			origin = `[[ env 'GITHUB_TOKEN']]`
+			expected = origin
+		})
+
+		It("should do nothing", func() {
+			Expect(gotten).To(Equal(expected))
+		})
+	})
+
+	When("config has quote", func() {
+		BeforeEach(func() {
+			origin = `[[ env "GITHUB_TOKEN"]]`
+			expected = origin
+		})
+
+		It("should do nothing", func() {
+			Expect(gotten).To(Equal(expected))
+		})
+	})
+
+	When("config has multiple variables", func() {
+		BeforeEach(func() {
+			origin = `[[ env GITHUB_TOKEN]]/[[ env "GITLAB_TOKEN"]]`
+			expected = `[[ env "GITHUB_TOKEN" ]]/[[ env "GITLAB_TOKEN"]]`
+		})
+
+		It("should succeed", func() {
+			Expect(gotten).To(Equal(expected))
+		})
+	})
+
+	When("the first word has quote", func() {
+		BeforeEach(func() {
+			origin = `[[ "env" GITHUB_TOKEN]]`
+			expected = origin
+		})
+
+		It("should do nothing", func() {
+			Expect(gotten).To(Equal(expected))
+		})
+	})
+
+	When("there is only one word", func() {
+		BeforeEach(func() {
+			origin = `[[GITHUB_TOKEN]]`
+			expected = origin
+		})
+
+		It("should do nothing", func() {
+			Expect(gotten).To(Equal(expected))
+		})
+	})
+})
