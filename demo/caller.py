23de0140d2e7a401ee21c31a560377f3c9169768
kagami <mingjing@merico.dev>
2025-03-12 20:05:23 +0800

    Rename package

--- demo/caller.py
+++ demo/caller.py
@@ -1,16 +1,16 @@
 import os
 
-# Set CHATFLOW_BASE to /demo/base
+# Set DEVSTREAM_BASE to /demo/base
 demo_base = os.path.join(os.path.dirname(__file__), "base")
-os.environ["CHATFLOW_BASE"] = demo_base
+os.environ["DEVSTREAM_BASE"] = demo_base
 
-import chatflow  # noqa: E402
+import devstream  # noqa: E402
 
 
 def main():
-    # res = chatflow.call("/hello_world", "--help")
+    # res = devstream.call("/hello_world", "--help")
     # print(res)
-    _ = chatflow.call("/hello_world", "This is some user input.")
+    _ = devstream.call("/hello_world", "This is some user input.")
 
 
 if __name__ == "__main__":
